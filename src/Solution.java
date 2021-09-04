93dc3725038fadf9755ed6569b87ebe14f8423fe
Yang Li <yangli@Yangs-MacBook-Pro-2.local>
2021-09-04 12:10:39 -0700

    ignore Solution.java

--- src/Solution.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
-class Solution {
-
-    public static void main(String[] args) {
-    }
-}
---
+++ src/graph/_834_sum_of_distances_in_tree.java
@@ -0,0 +1,5 @@
+package graph;
+
+public class _834_sum_of_distances_in_tree {
+
+}
